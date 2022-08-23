bcf0e448d7377d5067853e8b56683451dd4f5a38
Evan Rosson <rossonevan7281@gmail.com>
2022-08-23 13:09:34 -0500

    fixed textarea input

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,7 +6,7 @@ function Form ({addReview}) {
     return (
         <form className="review-submit">
             <input className="input" name="rating" type="number" min="0" max="5" step="1" placeholder="Insert Rating Here" required />
-            <input className="input" name="review" type='textarea' placeholder="Write Your Review Here" required />
+            <textarea className="input" name="review" type='text' placeholder="Write Your Review Here" maxLength='500' required />
             <input className="input" name="name" type="text" placeholder="Insert Name Here" required />
             <button type="submit" className="submit-button">Submit Review!</button>
         </form>
