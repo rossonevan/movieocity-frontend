f4c4af14912e8bcc56c667d41a956504c0a19282
Evan Rosson <rossonevan7281@gmail.com>
2022-08-23 13:34:21 -0500

    added post request

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,13 +2,33 @@ import React from "react";
 
 function Form ({addReview}) {
     
+    const [form, setForm] = useState([])
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        fetch('http://localhost:9292/reviews',
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                comment: comment,
+                rating: rating
+            })
+        })
+            .then((resp) => resp.json())
+            .then(newReview => addReview(newReview))
+    }
+
+    
 
     return (
         <form className="review-submit">
             <input className="input" name="rating" type="number" min="0" max="5" step="1" placeholder="Insert Rating Here" required />
             <textarea className="input" name="review" type='text' placeholder="Write Your Review Here" maxLength='500' required />
             <input className="input" name="name" type="text" placeholder="Insert Name Here" required />
-            <button type="submit" className="submit-button">Submit Review!</button>
+            <button onSubmit={handleSubmit} type="submit" className="submit-button">Submit Review!</button>
         </form>
     )
 }
